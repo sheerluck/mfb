e44883af1585078033e8edd6354aaf7e613add58
sheerluck <sheer.luck.andrew@gmail.com>
2017-08-24 12:25:17 +0300

    fix for pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=_fz+=%!j3r4v=uzuyn9zz@z@s^^h15$(0b)-a7n%jl5i%3eh9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'seerluck.pythonanywhere.com' ]
 
 
 # Application definition
